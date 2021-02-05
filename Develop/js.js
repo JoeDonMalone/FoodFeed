475c3dcedb1d130ec14f99949af2a0185cacd892
Vanity <ktkyletran@gmail.com>
2021-02-04 21:28:38 -0600

    added api calls and displays info (only one result for now)

--- Develop/js.js
+++ Develop/js.js
@@ -1,39 +1,61 @@
-var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=by-burrito&location=Torrance";
+var businessId = {
+    id: "",
+    phone: "",
+    category: "",
+    name: "",
+    rating: "",
+    location: {
+        address: "",
+        city: "",
+        state: "",
+        lat: "",
+        lng: "",
+    }
+}
 
 $.ajax({
-   url: myurl,
-   headers: {
-    'Authorization':'Bearer z2x4A0TyHJSAobgentP4RKrpDPevigDcn4a-KsQRcZKtNNlDy_3q5mewl5k3OmCymoRUc6RdcJil9iNmkKweXCdQ0E--l3OVgoekklGCh_v81frl5gSiLVo4mr4YYHYx',
-},
-   method: 'GET',
-   dataType: 'json',
-   success: function(data){
-       // Grab the results from the API JSON return
-       var totalresults = data.total;
-       // If our results are greater than 0, continue
-       if (totalresults > 0){
-           // Display a header on the page with the number of results
-           $('#results').append('<h5>We discovered ' + totalresults + ' results!</h5>');
-           // Itirate through the JSON array of 'businesses' which was returned by the API
-           $.each(data.businesses, function(i, item) {
-               // Store each business's object in a variable
-               var id = item.id;
-               var alias = item.alias;
-               var phone = item.display_phone;
-               var image = item.image_url;
-               var name = item.name;
-               var rating = item.rating;
-               var reviewcount = item.review_count;
-               var address = item.location.address1;
-               var city = item.location.city;
-               var state = item.location.state;
-               var zipcode = item.location.zip_code;
-               // Append our result into our page
-               $('#results').append('<div id="' + id + '" style="margin-top:50px;margin-bottom:50px;"><img src="' + image + '" style="width:200px;height:150px;"><br>We found <b>' + name + '</b> (' + alias + ')<br>Business ID: ' + id + '<br> Located at: ' + address + ' ' + city + ', ' + state + ' ' + zipcode + '<br>The phone number for this business is: ' + phone + '<br>This business has a rating of ' + rating + ' with ' + reviewcount + ' reviews.</div>');
-         });
-       } else {
-           // If our results are 0; no businesses were returned by the JSON therefor we display on the page no results were found
-           $('#results').append('<h5>We discovered no results!</h5>');
-       }
-   }
-});      
+    dataType: "json",
+    url: "https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=XMAVIEZPOP0WG0IT4JHIYOY04QJF4QHBKRUEJUWRYUEHID30&client_secret=LMYDERWOSS4OPZUAIZQTQWTZBHWLHHVAK4FHTDYZRPIVTAYF&v=20210204",
+    data: {},
+    success: function(data) {
+
+        businessId.id = data.response.venues[0].id;
+        businessId.category = data.response.venues[0].categories[0].name;
+        businessId.name = data.response.venues[0].name;
+        businessId.location.city = data.response.venues[0].location.city;
+        businessId.location.state = data.response.venues[0].location.state;
+        businessId.location.lat = data.response.venues[0].location.lat;
+        businessId.location.lng = data.response.venues[0].location.lng;
+
+        console.log(businessId.id);
+    }
+  });
+
+  $.ajax({
+    dataType: "json",
+    url: "https://api.foursquare.com/v2/venues/49eeaf08f964a52078681fe3?client_id=XMAVIEZPOP0WG0IT4JHIYOY04QJF4QHBKRUEJUWRYUEHID30&client_secret=LMYDERWOSS4OPZUAIZQTQWTZBHWLHHVAK4FHTDYZRPIVTAYF&v=20210204",
+    data: {},
+    success: function(data) {
+        businessId.phone = data.response.venue.contact.phone;
+        businessId.location.address = data.response.venue.location.address;
+        businessId.rating = (data.response.venue.rating/2);
+        displayBusiness();
+        console.log(data);
+    }
+  });
+
+  function displayBusiness(){
+    var idEl = document.querySelector("#results");
+
+    var name = document.createElement("h1");
+    var phoneNum = document.createElement("h5");
+    var address = document.createElement("h5");
+    var rating = document.createElement("h5");
+  
+    rating.textContent = `Rating: ${businessId.rating}`;
+    name.textContent = `${businessId.name}`;
+    phoneNum.textContent = `Phone Number: ${businessId.phone}`;
+    address.textContent = `Address: ${businessId.location.address}, ${businessId.location.city}, ${businessId.location.state}`;
+
+    idEl.append(name, rating, phoneNum, address);
+}
