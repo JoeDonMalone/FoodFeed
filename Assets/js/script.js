c5d7bb0fe151e8981884a7d63eba44bc6e2cd636
Joe Malone <joe.d.malone@gmail.com>
2021-02-07 22:40:30 -0600

    more html restructure and css applied to 'let's go'button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -344,22 +344,22 @@ $(document).ready(function() {
     }
   }
 
-  const getVenuePhotos = async (venueId) => {
-    const photoUrlToFetch =  photoUrl + venueId / '/photos?limit=1&client_id=' +  clientID + '&client_secret='  + secretID + '&v=20190903';
-    try {
-      const response = await fetch(photoUrlToFetch);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        const photo = jsonResponse.response.photos.items[0];
-        const photoUrl = `${photo.prefix}100x100${photo.suffix}`;
-        console.log(photoUrl);
-        return photoUrl;
-      }
-      throw new Error('Request failed!');
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const getVenuePhotos = async (venueId) => {
+  //   const photoUrlToFetch =  photoUrl + venueId / '/photos?limit=1&client_id=' +  clientID + '&client_secret='  + secretID + '&v=20190903';
+  //   try {
+  //     const response = await fetch(photoUrlToFetch);
+  //     if (response.ok) {
+  //       const jsonResponse = await response.json();
+  //       const photo = jsonResponse.response.photos.items[0];
+  //       const photoUrl = `${photo.prefix}100x100${photo.suffix}`;
+  //       console.log(photoUrl);
+  //       return photoUrl;
+  //     }
+  //     throw new Error('Request failed!');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
   function initializeSearchHistory() {
     let recentSearches = JSON.parse(localStorage.getItem('Recent Places Searches'));
@@ -404,40 +404,35 @@ $(document).ready(function() {
      }
     }
     $.getJSON(settings, function(data) {
-     data = data.restaurants;
-     var html = "";
-     $.each(data, function(index, value) {
-      var x = data[index];
-       console.log(typeof x);
-       console.log(x);
-      $.each(x, function(index, value) {
-        console.log(value.thumb);
-        console.log(value.url);
-       var location = x.restaurant.location;
-       var userRating = x.restaurant.user_rating;
-       html += "<div class='data img-rounded'>";
-       html += "<div class='rating'>";
-       html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
-       html += "  <strong class='text-info'>" + userRating.votes + " votes</strong>";
-       html += "</div>";
-       html += "<img  src=" + value.thumb + " alt='Restaurant Image' height='185' width='185'>";
-       html += "<a href=" + value.url + " target='_blank' class='action_link'><h2 style='color:red;'><strong>" + value.name + "</strong></h2></a>";
-       html += "  <strong class='text-primary'>" + location.locality + "</strong><br>";
-       html += "  <h6 style='color:grey;'><strong>" + location.address + "</strong></h6><hr>";
-       html += "  <strong>CUISINES</strong>: " + value.cuisines + "<br>";
-       html += "  <strong>COST FOR TWO</strong>: " + value.currency + value.average_cost_for_two + "<br>";
-       html += "</div><br>";
+      data = data.restaurants;
+      var html = "";
+      $.each(data, function(index, value) {
+        var x = data[index];
+        console.log(typeof x);
+        console.log(x);
+        $.each(x, function(index, value) {
+          console.log(value.thumb);
+          console.log(value.url);
+        var location = x.restaurant.location;
+        var userRating = x.restaurant.user_rating;
+        html += "<div class='data img-rounded'>";
+        html += "<div class='rating'>";
+        html += "<span title='" + userRating.rating_text + "'><p style='color:white;background-color:#" + userRating.rating_color + ";border-radius:4px;border:none;padding:2px 10px 2px 10px;text-align: center;text-decoration:none;display:inline-block;font-size:16px;float:right;'><strong>" + userRating.aggregate_rating + "</strong></p></span><br>";
+        html += "  <strong class='text-info'>" + userRating.votes + " votes</strong>";
+        html += "</div>";
+        html += "<img  src=" + value.thumb + " alt='Restaurant Image' height='185' width='185'>";
+        html += "<a href=" + value.url + " target='_blank' class='action_link'><h2 style='color:red;'><strong>" + value.name + "</strong></h2></a>";
+        html += "  <strong class='text-primary'>" + location.locality + "</strong><br>";
+        html += "  <h6 style='color:grey;'><strong>" + location.address + "</strong></h6><hr>";
+        html += "  <strong>CUISINES</strong>: " + value.cuisines + "<br>";
+        html += "  <strong>COST FOR TWO</strong>: " + value.currency + value.average_cost_for_two + "<br>";
+        html += "</div><br>";
+        });
+
       });
-     });
+
      $(".message").html(html);
     });
    }
-
-
-
-
-
-
-
 });
 
