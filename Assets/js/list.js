1d82e9fb8ecb5fd0cfc9db2234e26d1ba76aac53
Joe Malone <joe.d.malone@gmail.com>
2021-02-10 02:47:51 -0600

    updated layout to be mobile respsonsive, via flexbox, added relative distance, rating, name, to non-modal js populated cells

--- Assets/js/list.js
+++ Assets/js/list.js
@@ -1,7 +1,11 @@
-
 var categoryEl = $("#category option:selected");
-var selectedIndex;
+var id;
 var favorites; 
+
+var currentLocation = {
+   lat: "",
+   lon: ""
+}
 
 
 function displayBusiness(){
@@ -23,47 +27,119 @@ function displayBusiness(){
 var favorite;
 
 $(document).ready(function() {
-  favorites = localStorage.getItem("FavoritePlaces");
-  favorites = JSON.parse(favorites);
+   // getLocation();
+   handlePermission();
+   favorites = localStorage.getItem("FavoritePlaces");
+   favorites = JSON.parse(favorites);
+   if (favorites === null) {
 
-      if (favorites === null) { 
+   } else {
+      showFavoriteList();
+   }
 
-      } else {
-        console.log("in else ");
-
-        showFavoriteList();
+   $('.favorites-cell').hover(
+      function() {
+         $( this ).addClass( "hover" );
+         console.log('hover');
+      }, function() {
+         $( this ).removeClass( "hover" );
       }
+      );
 });
 
-function distance(lat1, lon1, lat2, lon2) {
-  if ((lat1 == lat2) && (lon1 == lon2)) {
-     return 0;
-  }
-  else {
-     var radlat1 = Math.PI * lat1/180;
-     var radlat2 = Math.PI * lat2/180;
-     var theta = lon1-lon2;
-     var radtheta = Math.PI * theta/180;
-     var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-     if (dist > 1) {
-        dist = 1;
+function handlePermission() {
+   navigator.permissions.query({name:'geolocation'}).then(function(result) {
+     if (result.state == 'granted') {
+       console.log(result.state);
+      //  geoBtn.style.display = 'none';
+     } else if (result.state == 'prompt') {
+      console.log(result.state);
+      //  geoBtn.style.display = 'none';
+       navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+     } else if (result.state == 'denied') {
+      console.log(result.state);
+      //  geoBtn.style.display = 'inline';
      }
-     dist = Math.acos(dist);
-     dist = dist * 180/Math.PI;
-     dist = dist * 60 * 1.1515;
-     dist = dist * 0.8684;
-     return dist.toFixed(2);
-  }
+     result.onchange = function() {
+      console.log(result.state);
+     }
+   });
+ }
+ 
+ function report(state) {
+   console.log('Permission ' + state);
+ }
+
+
+// function getLocation() {
+//    navigator.geolocation.getCurrentPosition(function(position) {
+//       let lat = position.coords.latitude;
+//       let long = position.coords.longitude;
+//       currentLocation.lat = lat;
+//       currentLocation.lon = long;
+//       // callWeatherInfo(lat, long);
+//    });
+// }
+
+// function distance(lat1, lon1, lat2, lon2) {
+//   if ((lat1 == lat2) && (lon1 == lon2)) {
+//      return 0;
+//   }
+//   else {
+//      var radlat1 = Math.PI * lat1/180;
+//      var radlat2 = Math.PI * lat2/180;
+//      var theta = lon1-lon2;
+//      var radtheta = Math.PI * theta/180;
+//      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+//      if (dist > 1) {
+//         dist = 1;
+//      }
+//      dist = Math.acos(dist);
+//      dist = dist * 180/Math.PI;
+//      dist = dist * 60 * 1.1515;
+//      dist = dist * 0.8684;
+//      return dist.toFixed(2);
+//   }
+// }
+
+function displayMapAt(lat, lon) {
+   $("#map")
+   .html(
+      "<iframe src=\"http://maps.google.com/maps?q=" + lat +  "," + lon + "&z=15&output=embed\"></iframe>");
+   }
+
+function distance(lat1, lon1, lat2, lon2) {
+   if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+   }
+   else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+         dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      dist = dist * 0.8684;
+      return dist.toFixed(2);
+   }
 }
+
+
 function clickCard(id) {
       
-  console.log("click " + parseInt(id));
-  selectedIndex = parseInt(id);
-  console.log(favorites[selectedIndex].restaurant.name);
+//   console.log("click " + parseInt(id));
+  id = parseInt(id);
+//   console.log(favorites[id].restaurant.name);
   
   var modal = $("#modalDetail");
   var name = modal.find( "#name");
-  var cusine = modal.find( "#modalCuisine");
+  var cuisine = modal.find( "#modalCuisine");
+  var highlights = modal.find( "#highlights");
   var price = modal.find( "#modalPrice");
   var rating = modal.find( "#modalRatingText");
   var address = modal.find( "#modaladdres"); 
@@ -74,27 +150,25 @@ function clickCard(id) {
   var delivery = modal.find( "#modalDelivery");
   var favButton = modal.find( "#modalFavorite");
 
-  console.log(favorites[selectedIndex].restaurant.name);
-  console.log(favorites[selectedIndex].restaurant.cuisines);
-
-  cusine.text(favorites[selectedIndex].restaurant.cuisines);
-  name.text(favorites[selectedIndex].restaurant.name);
-  rating.text(favorites[selectedIndex].restaurant.user_rating.aggregate_rating);
-  address.text(favorites[selectedIndex].restaurant.location.address);
-  phone.text(favorites[selectedIndex].restaurant.phone_numbers);
-  timing.text(favorites[selectedIndex].restaurant.timings);
-  offers.text(favorites[selectedIndex].restaurant.offers);
+  cuisine.text(favorites[id].restaurant.cuisines);
+  name.text(favorites[id].restaurant.name);
+  highlights.text(favorites[id].restaurant.highlights);
+  rating.text(favorites[id].restaurant.user_rating.aggregate_rating);
+  address.text(favorites[id].restaurant.location.address);
+  phone.text(favorites[id].restaurant.phone_numbers);
+  timing.text(favorites[id].restaurant.timings);
+  offers.text(favorites[id].restaurant.offers);
   
-  displayMapAt(favorites[selectedIndex].restaurant.location.latitude, favorites[selectedIndex].restaurant.location.longitude);
+  displayMapAt(favorites[id].restaurant.location.latitude, favorites[id].restaurant.location.longitude);
   let pricetext = "";
   
-  for (var i=0; i<parseInt(favorites[selectedIndex].restaurant.price_range); i++) {
+  for (var i=0; i<parseInt(favorites[id].restaurant.price_range); i++) {
      pricetext += "$";
   }
   price.text(pricetext);
   
   
-  let hasdelivery = favorites[selectedIndex].restaurant.has_online_delivery;
+  let hasdelivery = favorites[id].restaurant.has_online_delivery;
   
   if (hasdelivery == "1") {
      delivery.text(" :YES");
@@ -102,7 +176,7 @@ function clickCard(id) {
      delivery.text(" :NO");
      
   }
-  var highlights = favorites[selectedIndex].restaurant.highlights;
+  var highlights = favorites[id].restaurant.highlights;
   if (highlights.length > 0 ){
      let text = "";
      for (var i=0 ; i<highlights.length; i++) {
@@ -115,7 +189,7 @@ function clickCard(id) {
 
   favButton.find("i").removeClass().addClass("fa fa-thumbs-up");
 
-  /*if (checkIfItemIsFavorite(savedData, result[selectedIndex].restaurant.id)) {
+  /*if (checkIfItemIsFavorite(savedData, result[id].restaurant.id)) {
      favButton.find("i").removeClass().addClass("fa fa-thumbs-up");
   } else {
      favButton.find("i").removeClass().addClass("fa fa-thumbs-down");
@@ -129,28 +203,46 @@ function showFavoriteList() {
   var name = "";
   var cuisines = "";
   var rating = "";
-  var icon = "";
+  var icon;
   var favButton = "";
+  var location;
   console.log(favorites[0]);
 
   $.each(favorites, function (i) {
-    favorites[i] = favorites[i].restaurant;
-    var dist = "";
-     //var dist = distance(response[i].restaurant.location.latitude,response[i].restaurant.location.longitude, currentLocation.lat, currentLocation.lon );
+     favorites[i] = favorites[i].restaurant;
+     var dist = "";
+     var dist = distance(favorites[i].restaurant.location.latitude,favorites[i].restaurant.location.longitude, currentLocation.lat, currentLocation.lon );
      name = favorites[i].restaurant.name;
      cuisines = favorites[i].restaurant.cuisines;
      rating = favorites[i].restaurant.user_rating.aggregate_rating;
      icon = favorites[i].restaurant.thumb;
      if (icon == "" || icon == null) {
-        icon = "./Assets/images/img.jpeg";
+      icon = './Assets/images/img.png';
      }
-    
-     
-     if (i%3 == 0) {
-        string += '<div class="grid-x small-up-2 medium-up-3">';
-     }
-     string += '<div class="cell"> <div class="card" onclick="clickCard(this.id)" data-open="modalDetail" id='+ i + '"> <div class= "text-center">  <img class="card-image" src=' + icon + '> </div> <h6 class="card-title">' + name + '</h6> <p> <span class="card-cuisine">' + cuisines + '<br> </span> <span class="card-rating">  ' + rating +   '</span> <span class="card-dist">' + dist+ ' miles </span> </p> <button type="button" class="success button">Save</button> <i class="fa fa-map-marker"></i><i class="fa fa-heart-o" aria-hidden="true"></i>   test  </button>  </div> </div>';
+      
+      
+   //   if (i%3 == 0) {
+   //      string += '<div class="grid-x small-up-2 medium-up-3">';
+   //   }
+      string += 
+      `<div class="favorites-cell"> 
+         <div class="flex-card" onclick="clickCard(this.id)" data-open="modalDetail" id=${i}"> 
+            <div class= "flex-pic-center">  
+               <img class="flex-card-image" src=${icon}>
+            </div> 
+            <div class = "favorites-details-column">
+               <h6 class="flex-card-title"><strong>${name}</strong></h6> 
+               <p> 
+                  <span class="card-cuisine">${cuisines}</span> <br>
+                  <span class="card-rating"><strong>Rating: ${rating} </strong></span>
+                  <span class="card-dist">${dist} miles </span> 
+               </p> 
+               <button type="button" class="success button">Remove</button>
+            </div>
+         </div> 
+      </div>`;
   })
   
   $('#card').append(string);
 }
+
